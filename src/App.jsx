0a29519cb9f7865810618fe77602992bc55b41a6
vikasthangavel <vikasthangavel@gmail.com>
2026-06-15 21:27:41 +0530

    chatbot isues

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Footer from "./components/Footer";
 import QuickSummary from "./components/QuickSummary";
 import { useAnalytics } from "./hooks/useAnalytics";
 import AdminStats from "./components/AdminStats";
-import ChatBot from "./components/Chatbot.jsx";
+import ChatBot from "./components/Chatbot";
 
 
 function App() {
